9b9ccd25786eb7b799c9f7cea2ba35b2d4bff01b
Korn Kutan <korn@kutan.me>
2013-02-07 23:37:25 +0700

    initialize and import source code first time

---
+++ JAXWS-Spring/src/main/java/th/co/mwa/cis/business/Hello.java
@@ -0,0 +1,11 @@
+package th.co.mwa.cis.business;
+
+public interface Hello {
+
+	public String sayHello();
+
+	public String sayGoodbye();
+
+	public String helloFullname(String firstname, String lastname);
+
+}
---
+++ JAXWS-Spring/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+	<body>
+		<h2>MWA CIS Web Services by using JAX-WS + Spring Integration</h2>
+	</body>
+</html>
